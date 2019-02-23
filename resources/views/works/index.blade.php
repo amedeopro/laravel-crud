0c3f03252d4a21c71a94c4dd802df63a7eaa8f4f
amedeopro <amedeopro@me.com>
2019-02-23 21:26:18 +0100

    creata rotta CREATE e anche pagina views

--- resources/views/works/index.blade.php
+++ resources/views/works/index.blade.php
@@ -8,6 +8,8 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
+        <h1>Questi sono tutti i lavori da svolgere <a href="{{ route('works.create')}}" class="btn btn-primary">Aggiungi un nuovo lavoro</a></h1>
+
         <table class="table">
           <thead class="thead-dark">
             <tr>
@@ -23,10 +25,10 @@
           <tbody>
             @foreach ($works as $work)
               <tr>
-                <th scope="row">{{ $work['id'] }}</th>
-                <td>{{ $work['cliente'] }}</td>
-                <td>{{ $work['lavoro'] }}</td>
-                <td>{{ $work['assegnazione'] }}</td>
+                <th scope="row">{{ $work->id }}</th>
+                <td>{{ $work->cliente }}</td>
+                <td>{{ $work->lavoro }}</td>
+                <td>{{ $work->assegnazione }}</td>
                 <td><a href="#" class="btn btn-info">Visualizza</a></td>
                 <td><a href="#" class="btn btn-warning">Modifica</a></td>
                 <td><a href="#" class="btn btn-danger">Elimina</a></td>
