99a2b254b4743d1e5500e4acb0087333ef60717f
amedeopro <amedeopro@me.com>
2019-02-23 21:15:04 +0100

    index ok

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,10 +1,15 @@
 <div class="menu">
-  <div class="row">
-    <div class="menu-left">
+  <div class="container">
 
-    </div>
-    <div class="menu-right">
-      
+    <div class="row">
+      <div class="menu-left">
+        <img src="{{ asset('img/logo.png')}}" alt="">
+      </div>
+      <div class="menu-right">
+        <a href="#">Pulsante 1</a>
+        <a href="#">Pulsante 2</a>
+        <a href="#">Pulsante 3</a>
+      </div>
     </div>
   </div>
 </div>
