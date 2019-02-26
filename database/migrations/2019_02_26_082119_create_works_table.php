98624890bb183944eae5c5f6133d1a9e1c381453
amedeopro <amedeopro@me.com>
2019-02-26 09:22:35 +0100

    ripristinata tabella works

---
+++ database/migrations/2019_02_26_082119_create_works_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('cliente',30);
+            $table->string('lavoro',250);
+            $table->string('assegnazione',30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+}
